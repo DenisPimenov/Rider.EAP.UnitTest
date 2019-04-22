7a2ab0760139f111df5a339b7f113ccf68bf1739
denispimenov <pimenovdenis.prog@gmail.com>
2019-04-22 09:58:05 +0300

    upload project

---
+++ Test.cs
@@ -0,0 +1,25 @@
+﻿using AutoFixture.Xunit2;
+using Xunit;
+
+namespace Rider.EAP.UnitTest
+{
+
+    class Foo
+    {
+        public string Do(string a)
+        {
+            return a;
+        }
+    }
+    
+    public class Test
+    {
+        [Theory]
+        [AutoData]
+        public void TestAutoData(string a)
+        {
+            
+            Assert.True(new Foo().Do(a).Length > 0);
+        }
+    }
+}
